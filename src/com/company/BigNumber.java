8ac5456298f9ea9e01953120c70d0197259bac9a
devonshiw5 <devonshiw5@students.rowan.edu>
2018-09-30 18:13:11 -0400

    Initial Commit

---
+++ src/com/company/BigNumber.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class BigNumber {
+
+    private ArrayList<Character> bigNumber;
+
+    public BigNumber(String number){
+
+    }
+
+    public BigNumber add(BigNumber number){
+
+    }
+
+    public BigNumber subtract(BigNumber number){
+
+    }
+
+    public BigNumber multiply(BigNumber number){
+
+    }
+
+    public BigNumber divide(BigNumber number){
+
+    }
+
+    public BigNumber negate(){
+
+    }
+
+    public int sign(){
+
+    }
+
+    public void normalize(){
+
+    }
+
+
+}
